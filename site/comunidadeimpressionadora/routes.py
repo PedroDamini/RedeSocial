8f09cf82dff1f224fd8521d0713acbb38ff56b9b
unknown <pedrodelemosdamini@gmail.com>
2023-09-01 21:42:59 -0300

    DEV

--- site/comunidadeimpressionadora/routes.py
+++ site/comunidadeimpressionadora/routes.py
@@ -72,6 +72,8 @@ def perfil():
 def criar_post():
     return render_template('criarpost.html')
 
+############################################################################################################################################
+
 def salvar_imagem(imagem):
     codigo = secrets.token_hex(8)
     nome, extensao = os.path.splitext(imagem.filename)
@@ -85,6 +87,16 @@ def salvar_imagem(imagem):
     imagem_reduzida.save(caminho_completo)
     return nome_arquivo
 
+def atualizar_cursos(form):
+    lista_cursos = []
+    for campo in form:
+        if 'curso_' in campo.name:
+            if campo.data:
+                lista_cursos.append(campo.label.text)
+    return ';'.join(lista_cursos)
+
+############################################################################################################################################
+
 @app.route("/perfil/editar", methods=['GET', 'POST'])
 @login_required
 def editar_perfil():
@@ -96,6 +108,7 @@ def editar_perfil():
             nome_imagem = salvar_imagem(form.foto_perfil.data)
             current_user.foto_perfil = nome_imagem
 
+        current_user.cursos = atualizar_cursos(form)
         database.session.commit()
         flash(f'perfil atualizado com sucesso', 'alert-success')
         return redirect(url_for('perfil'))
