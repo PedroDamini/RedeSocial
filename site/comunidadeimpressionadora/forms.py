1e347e1217ae81b26277db347f56a44f5a2a2581
unknown <pedrodelemosdamini@gmail.com>
2023-09-06 16:57:09 -0300

    DEV

--- site/comunidadeimpressionadora/forms.py
+++ site/comunidadeimpressionadora/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError  
 from comunidadeimpressionadora.models import Usuario 
 from flask_login import current_user
@@ -45,3 +45,9 @@ class FormEditarPerfil(FlaskForm):
             usuario = Usuario.query.filter_by(email=email.data).first()
             if usuario:
                 raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail')
+            
+            
+class FormCriarPost(FlaskForm):
+    titulo = StringField('Titulo do Post', validators=[DataRequired(), Length(1, 150)])
+    corpo = TextAreaField('Escreva seu Post aqui', validators=[DataRequired()])
+    botao_submit = SubmitField('Criar Post')
--- site/comunidadeimpressionadora/routes.py
+++ site/comunidadeimpressionadora/routes.py
@@ -1,7 +1,7 @@
 from comunidadeimpressionadora import app, database, bcrypt
 from flask import render_template, redirect, url_for, flash, request
-from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil
-from comunidadeimpressionadora.models import Usuario
+from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormCriarPost
+from comunidadeimpressionadora.models import Usuario, Post
 from flask_login import login_user, logout_user, current_user, login_required
 import secrets
 import os
@@ -70,8 +70,14 @@ def perfil():
 @app.route("/post/criar")
 @login_required
 def criar_post():
-    return render_template('criarpost.html')
-
+    form = FormCriarPost()
+    if form.validate_on_submit():
+        post = Post(titulo=form.titulo.data, corpo=form.corpo.data, autor=current_user)
+        database.session.add(post)
+        database.session.commit
+        flash('Post Criado com sucesso', 'alert-success')
+        return redirect(url_for('home'))
+    return render_template('criarpost.html', form=form)
 
 @app.route("/perfil/editar", methods=['GET', 'POST'])
 @login_required
