556741211ed4eb51285495ba00f92e272825cc77
unknown <pedrodelemosdamini@gmail.com>
2023-09-17 01:43:04 -0300

    ajuste adicao banco online com varenv

--- site/Piplus/__init__.py
+++ site/Piplus/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template, url_for, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = '221b5d887efb423650e73b9c65466aae'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
