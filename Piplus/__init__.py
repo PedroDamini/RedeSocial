ac745929ce1b3fa49a4f1ef4cc75957afd082429
unknown <pedrodelemosdamini@gmail.com>
2023-09-17 03:13:10 -0300

    ajuste adicao config BD

--- Piplus/__init__.py
+++ Piplus/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -19,5 +20,16 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message = 'faça login para continuar'
 login_manager.login_message_category = 'alert-warning'
+
+from Piplus import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("base de dados criada")
+else:
+    print("base de dados ja existente")
 
 from Piplus import routes
